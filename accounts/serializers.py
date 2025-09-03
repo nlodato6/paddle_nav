95175d75dcbee90833cad8953c824081cec91454
Nicole Lodato <nicashlod@gmail.com>
2025-09-02 21:51:59 -0400

    removed duplicates

--- accounts/serializers.py
+++
@@ -1,9 +0,0 @@
-# accounts/serializers.py
-from rest_framework.serializers import ModelSerializer
-from django.contrib.auth.models import User
-
-
-class SignupSerializer(ModelSerializer):
-    class Meta:
-        model = User
-        fields = ["username", "password"]
