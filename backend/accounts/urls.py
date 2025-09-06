ef19759d090f772e6fd49b86a61927cce315b967
Nicole Lodato <nicashlod@gmail.com>
2025-09-06 13:25:02 -0400

    accounts-- api-token-auth/ added

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -5,5 +5,6 @@ from .views import SignupView
 
 urlpatterns = [
     path('get-token', obtain_auth_token),
-    path('signup', SignupView.as_view())
+    path('signup', SignupView.as_view()),
+    path('api-token-auth/', obtain_auth_token, name='api_token_auth'),
 ]
