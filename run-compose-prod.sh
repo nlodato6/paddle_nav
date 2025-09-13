9313885e8ca1cedf4d68f5ba80b33093da3cae12
Nicole Lodato <nicashlod@gmail.com>
2025-09-13 16:06:11 -0400

    1.1.2

--- run-compose-prod.sh
+++ run-compose-prod.sh
@@ -13,8 +13,8 @@ echo "Applying database migrations..."
 docker-compose -f docker-compose.prod.yml exec -T api python manage.py migrate
 
 
-# Echo "Copying sql files to the container..."
-# docker cp ./backend/init.sql paddle_nav-db-1:/tmp/init.sql
+Echo "Copying sql files to the container..."
+docker cp ./backend/init.sql paddle_nav-db-1:/tmp/init.sql
 
-# echo "Importing init.sql data to the database..."
-# docker exec -it paddle_nav-db-1 psql -U postgres -d paddle_nav_db -f /tmp/init.sql
+echo "Importing init.sql data to the database..."
+docker exec -it paddle_nav-db-1 psql -U postgres -d paddle_nav_db -f /tmp/init.sql
