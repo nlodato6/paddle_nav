76ae91b20bd890b1fe947038622a5d536a578a1f
Nicole Lodato <nicashlod@gmail.com>
2025-09-13 12:57:34 -0400

    trying again v2

--- run-compose-prod.sh
+++ run-compose-prod.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 # Build and start containers using production compose file
-docker-compose -f docker-compose.prod.yml pull
-docker-compose -f docker-compose.prod.yml up -d
+docker compose -f docker-compose.prod.yml pull
+docker compose -f docker-compose.prod.yml up -d
 
 # make sure the postgres container is ready, then run migrations
 sleep 10
-docker-compose -f docker-compose.prod.yml exec api python manage.py makemigrations
-docker-compose -f docker-compose.prod.yml exec api python manage.py migrate
+docker compose -f docker-compose.prod.yml exec api python manage.py makemigrations
+docker compose -f docker-compose.prod.yml exec api python manage.py migrate
