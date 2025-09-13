413899e7a222bb3f9a99e78b6f75b1e0ce5ea848
Nicole Lodato <nicashlod@gmail.com>
2025-09-13 13:57:09 -0400

    run-compose update

--- run-compose-prod.sh
+++ run-compose-prod.sh
@@ -4,7 +4,21 @@
 docker-compose -f docker-compose.prod.yml pull
 docker-compose -f docker-compose.prod.yml up -d
 
-# make sure the postgres container is ready, then run migrations
 
+echo "Creating new migrations for app..."
 docker-compose -f docker-compose.prod.yml exec -T api python manage.py makemigrations
+
+echo "Applying database migrations..."
 docker-compose -f docker-compose.prod.yml exec -T api python manage.py migrate
+
+
+Echo "Copying sql files to the container..."
+docker cp ./backend/init.sql paddle_nav-db-1:/tmp/init.sql
+
+echo "Importing init.sql data to the database..."
+docker exec -it paddle_nav-db-1 psql -U postgres -d paddle_nav_db -f /tmp/init.sql
+
+
+sleep 5
+
+Echo 'DB loaded'
